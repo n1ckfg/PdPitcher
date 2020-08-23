182188dd4bdf78e5e2952f020ab397d81547127d
n1ckfg <nick@fox-gieg.com>
2020-08-23 16:56:17 -0400

    shell scripts

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 PATCH_NAME=PdPitcher.pd
-EXT_MIDI='OP-1 Midi Device'
 
-#sleep 5 &
 xvfb-run pd $1 -rt -open $PATCH_NAME &
-#sleep 5 &
-sudo aconnect 'Pure Data':1 $EXT_MIDI:0
+sleep 5 &
+sudo aconnect 'Pure Data':1 'OP-1 Midi Device':0
 
 
 
