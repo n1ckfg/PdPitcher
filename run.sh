bf6d4524ef415778a51c3a810be055bc4d7aed0a
Nick Fox-Gieg <nick@fox-gieg.com>
2020-08-23 14:30:36 -0400

    shell scripts

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 PATCH_NAME=PdPitcher.pd
+EXT_MIDI='OP-1 Midi Device'
 
 #sleep 5 &
-pd $1 -rt -open $PATCH_NAME &
+xvfb-run pd $1 -rt -open $PATCH_NAME &
 #sleep 5 &
-sudo aconnect 'Pure Data':1 'TD-3':0
+sudo aconnect 'Pure Data':1 $EXT_MIDI:0
 
 
 
